53c359980e4291eddc2b82f4e5705b62c1fe6cd8
toufiq-dev <toufiq.dev@gmail.com>
2024-01-04 01:46:21 +0600

    fix: some modules

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -9,6 +9,7 @@ import { Observable, map, tap } from 'rxjs';
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('before reqss');
     return next.handle().pipe(map((data) => ({ data })));
   }
 }
